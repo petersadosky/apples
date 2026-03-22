6e2d3f4607c72fd7116c85980acad2d2f7cf7fca
test <test>
2026-03-21 22:13:38 -0400

    Initial Commit

---
+++ apples/applesApp.swift
@@ -0,0 +1,17 @@
+//
+//  applesApp.swift
+//  apples
+//
+//  Created by Peter Sadosky on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct applesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ applesTests/applesTests.swift
@@ -0,0 +1,17 @@
+//
+//  applesTests.swift
+//  applesTests
+//
+//  Created by Peter Sadosky on 3/21/26.
+//
+
+import Testing
+@testable import apples
+
+struct applesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
